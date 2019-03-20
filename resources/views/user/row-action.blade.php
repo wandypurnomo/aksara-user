28af96252ea3756f1e9e64b5ff95cda28ef4bf77
Havil Wintas Ernanda <havilwintas@tonjoo.com>
2019-03-20 11:20:26 +0700

    change destroy user icon

--- resources/views/user/row-action.blade.php
+++ resources/views/user/row-action.blade.php
@@ -2,6 +2,6 @@
   <a href="{{ $edit_url }}" class="btn btn-xs btn-default"><i title="Edit" class="fa fa-pencil"></i> </a>
 @endif
 @if(has_capability('delete-user'))
-  <a onclick='return confirm("{{ __('user::messages.confirm_delete_user') }}");' href="{{ $delete_url }}" class="btn btn-xs btn-default"><i title="Trash" class="fa fa-eye"></i></a>
+  <a onclick='return confirm("{{ __('user::messages.confirm_delete_user') }}");' href="{{ $delete_url }}" class="btn btn-xs btn-default"><i title="Trash" class="fa fa-trash"></i></a>
 @endif
 
